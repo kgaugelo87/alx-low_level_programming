137facb6c8cbfaabe2acbecb225f60c45e7cde4d
Kgaogelo <kgaugelo.maile@gmail.com>
2023-07-13 12:35:10 -0700

    C program that prints string output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -to print string output using puts
+ *
+ * Program should always end with (0)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
