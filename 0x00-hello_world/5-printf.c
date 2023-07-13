2f9f5695742a03a6b9332a929f3f30bd03bb46d9
Kgaogelo <kgaugelo.maile@gmail.com>
2023-07-13 12:51:12 -0700

    C program that prints string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main -function to print out the exact string provided
+ *
+ * Return: always return the value (0)
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
