5ed1c67d925f3579c57006b3227d6f911f87bc68
Kgaogelo <kgaugelo.maile@gmail.com>
2023-07-18 12:59:41 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * times_table - printing the times table
+ *
+ * Return: always return nothing
+ *
+ */
+void times_table(void)
+{
+	int i, j, k, l, m;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			k = i * j;
+			if (k > 9)
+			{
+				l = k % 10;
+				m = (k - l) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(m + '0');
+				_putchar(l + '0');
+			}
+			else
+			{
+				if (j != 0)
+				{
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
+				}
+				_putchar(k + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
