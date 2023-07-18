5536b35cef9d7b251e309c5a7a139ba528473ca4
Kgaogelo <kgaugelo.maile@gmail.com>
2023-07-18 13:05:22 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adding two number together
+ * @num1: first number
+ * @num2: second number
+ * Return: return a vlue
+ *
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
