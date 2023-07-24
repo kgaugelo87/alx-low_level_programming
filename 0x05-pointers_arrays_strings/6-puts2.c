f0644c845034561a11f0b58509e65c1b6c6574e1
Kgaogelo <kgaugelo.maile@gmail.com>
2023-07-24 13:40:23 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: our string value
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+	int j = 0;
+	char *k = str;
+	int l;
+
+	while (*k != '\0')
+	{
+		k++;
+		i++;
+	}
+	j = i - 1;
+	for (l = 0; l <= j; l++)
+	{
+		if (l % 2 == 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
