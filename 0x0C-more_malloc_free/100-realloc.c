1c714ec3a12d3dd636dd42faa554495493c79545
Kgaogelo <kgaugelo.maile@gmail.com>
2023-08-11 04:13:13 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,9 +16,10 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (ptr == NULL)
 	{
-		new_ptr = malloc(old_size);
+		new_ptr = malloc(new_size);
 
 		if (new_ptr == NULL)
 			return (NULL);
