faad21eb39214ef79778d9155376554b30fbdf0b
Kgaogelo <kgaugelo.maile@gmail.com>
2023-08-14 03:50:40 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ * struct dog - a dog's basic info
+ * @name: our dog's name
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
