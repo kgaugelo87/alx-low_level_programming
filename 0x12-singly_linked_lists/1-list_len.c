e020cd8467b024589057cedf19aa98df2fe9e7f9
kgaugelo87 <kgaugelo.maile@gmail.com>
2023-08-24 12:48:28 -0700

    List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,9 +3,9 @@
 
 /**
  * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
+ * @h: our pointer
+ * Return: number of elements
  *
- * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
