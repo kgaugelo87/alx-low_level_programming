d091b75acf23449a75ea14a1babf7d115fa3c067
kgaugelo87 <kgaugelo.maile@gmail.com>
2023-08-29 07:16:35 -0700

    Free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *node, *temp;
+	listint_t *node;
 
 	if (head == NULL)
 		return;
@@ -16,9 +16,8 @@ void free_listint2(listint_t **head)
 	node = *head;
 	while (node)
 	{
-		temp = node;
 		node = node->next;
-		free(temp);
+		free(*head);
 	}
 	*head = NULL;
 }
