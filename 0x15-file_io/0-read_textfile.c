01d21a31eb58bc82f00ef08b65048343de3385ac
kgaugelo87 <kgaugelo.maile@gmail.com>
2023-09-04 07:00:09 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,11 @@
 #include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -9,17 +16,18 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
 	char *buff;
-	ssize_t fd, fw, fr;
+	ssize_t fw, fr;
 
+	if (filename == NULL)
+		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
 		return (0);
-	}
 	buff = malloc(sizeof(char) * letters);
 	fr = read(fd, buff, letters);
-	fw = write(STDOUT_FILENO, buff, fr;
+	fw = write(STDOUT_FILENO, buff, fr);
 
 	free(buff);
 	close(fd);
