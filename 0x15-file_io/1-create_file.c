febbba55671ad57019924c702e9a43fcdd1501d1
kgaugelo87 <kgaugelo.maile@gmail.com>
2024-02-17 04:18:38 -0800

    Creating a text file if it doesnt exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	int length = 0;
 	int wrt;
 
-	if (filename != NULL)
+	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
 	{
