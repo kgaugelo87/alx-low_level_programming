6ac33ed5a830fceed7442b93b660b672cad809d3
kgaugelo87 <kgaugelo.maile@gmail.com>
2024-02-17 02:34:04 -0800

    Reading and printing to a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,7 +18,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t wrt;
 	ssize_t rd;
 
-	fid = fopen(filename, O_RDONLY);
+	fid = open(filename, O_RDONLY);
 	if (fid == -1)
 		return (0);
 
