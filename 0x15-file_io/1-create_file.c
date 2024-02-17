05c2fcdc643effce14d4f13b6309895e1e424dab
kgaugelo87 <kgaugelo.maile@gmail.com>
2024-02-17 04:17:05 -0800

    Creating a text file if it doesnt exist

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 			length++;
 	}
 
-	fid = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	fid = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wrt = write(fid, text_content, length);
 
 	if (fid == -1 || wrt == -1)
